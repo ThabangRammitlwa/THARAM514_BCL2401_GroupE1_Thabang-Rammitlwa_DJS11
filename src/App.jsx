97e4f4e57ea0d2410a401d9b25bf4202216da629
Thabang Rammitlwa <thabang451@gmail.com>
2024-06-19 10:27:06 +0200

    Removed Season file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Show from './components/Show';
 import Genre from './components/Genre';
-import Season from './components/Season';
 import Episode from './components/Episode';
 import ShowList from './components/ShowList';
 import AudioPlayer from './components/AudioPlayer';
@@ -12,7 +11,6 @@ function App() {
     <Routes>
       <Route path="/" element={<ShowList />} />
       <Route path="/show/:showId" element={<Show />} />
-      <Route path="/season/:seasonId" element={<Season />} />
       <Route path="/episode/:episodeId" element={<Episode />} />
       <Route path="/genre/:genreId" element={<Genre />} />
     </Routes>
